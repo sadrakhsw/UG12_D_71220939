01d618929821eb90706a2cf9bb517b056cf1c8aa
sadrakhsw <117900802+sadrakhsw@users.noreply.github.com>
2022-12-08 16:21:45 +0700

    Add files via upload

---
+++ 2_UG11_71220939.py
@@ -0,0 +1,11 @@
+def katapertama(lnama):
+    return nama[-3:]
+
+nama=(input("masukan kata : "))
+print("Huruf paling ujung pada kata", nama, "adalah", (nama[-3:]))
+
+def katakedua(lnama1):
+    return nama1[:3]
+
+nama1=input("masukan kata : ")
+print("Huruf paling ujung pada kata", nama1, "adalah", (nama1[:3]))
