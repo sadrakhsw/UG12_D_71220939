594f9eb1c1c7c5da7779bcb9e8b6d193187e5537
sadrakhsw <117900802+sadrakhsw@users.noreply.github.com>
2022-12-08 16:38:02 +0700

    Add files via upload

--- 2_D_71220939.py
+++ 2_D_71220939.py
@@ -1,13 +1,13 @@
 print("~~ Table Matematika Nich ~~~")
 print("Pilihan model matematika")
 print("1. Perkalian \n2. Pembagian")
-barang = int(input("Masukkan model matematika yang diinginkan 1/2 :"))
-if barang == 1:
+pilihan = int(input("Masukkan model matematika yang diinginkan 1/2 :"))
+if pilihan == 1:
     angka = int(input("Masukkan table matematika dari angka :"))
     for i in range(1,11):
             hasil = angka * i
             print(angka, "x", i, '=',hasil)
-elif barang == 2:
+elif pilihan == 2:
     angka1 = int(input("menampilkan table matematika dengan angka :"))
     for i in range(50,66):
         hasil1 = i / angka1
